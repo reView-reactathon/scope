9010502724dd3721ccebdbdfa07e6958d15449ec
JR Meunrath <jmeunrath@overstock.com>
2017-03-11 14:48:29 -0800

    added UI templates

---
+++ my-app/src/components/Language/Language.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Language.css';
+
+const Language = () => {
+    return (
+        <div className="language-wrapper">
+        <div className="mdl-grid">
+            <div className="mdl-cell mdl-cell--4-col">
+                <h3>Select Language</h3>
+            </div>
+        </div>
+
+        </div>
+    )
+}
+
+export default Language;
