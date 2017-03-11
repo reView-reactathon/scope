a9033fd39564bc56002e5c7d7b7b01323d76b5d1
Arturo Romero <aromero@digitalguardian.com>
2017-03-11 13:40:58 -0700

    remove frontend directory

--- frontend/src/StatusBoard.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import StatusBoard from './StatusBoard';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<StatusBoard />, div);
-});
--- frontend/src/utils/github-urls.js
+++
@@ -1,9 +0,0 @@
-
-export function githubIssueURL(number, repo) {
-  return `https://github.com/${repo}/issues/${number}`
-}
-
-export function githubUserImage(userID, size) {
-  const s = size || 460
-  return `https://avatars3.githubusercontent.com/u/${userID}?v=3&s=${s}`
-}
