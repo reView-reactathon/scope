df6bd627ac69bee619baaca60eb2b512ec0f5816
JR Meunrath <jmeunrath@overstock.com>
2017-03-11 15:33:31 -0800

    add language

--- my-app/src/components/Language/Language.js
+++ my-app/src/components/Language/Language.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import devlogo from '../../../assets/dev-icon.svg';
 import './Language.css';
 
 const Language = () => {
     return (
         <div className="language-wrapper">
-        <div className="mdl-grid">
-            <div className="mdl-cell mdl-cell--4-col">
-                <h3>Select Language</h3>
+            <div className="mdl-grid">
+                <div className="mdl-cell mdl-cell--4-col">
+                    <h3>
+                        <img src={devlogo} alt="dev logo"/> Select Language
+                    </h3>
+                </div>
             </div>
-        </div>
-
         </div>
     )
 }
