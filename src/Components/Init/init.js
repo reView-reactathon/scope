3e99bb14c178a212d36fbea8320d37b531aaad64
Claudiu Andrei <claudiu.andrei@docusign.com>
2017-03-12 15:04:51 -0700

    Add routing

--- src/Components/Init/init.js
+++ src/Components/Init/init.js
@@ -4,37 +4,32 @@ import StartInterview from '../StartInterview/StartInterview'
 import QuestionForm from '../Questions/QuestionForm';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 class Init extends Component {
-    constructor() {
-        super()
-        this.state = {
-            isSelected: false
-        }
-    }
-
-    selectLanguage() {
-        this.setState({isSelected: true});
-    }
-
-    toggleComponent() {
-        if (!this.state.isSelected) {
-            return <Language
-            languageEvent={this.selectLanguage.bind(this)}
-            isSelected={this.state.isSelected}
-            />
-        } else {
-            return <StartInterview />
-        }
-    }
-
     render() {
         return (
           <div className="App">
             <div className="App-header">
               <h2>Welcome to ReView</h2>
             </div>
-            <QuestionForm />
-            { this.toggleComponent() }
+            <Router>
+                <div>
+                  <ul>
+                    <li><Link to="/">Languages</Link></li>
+                    <li><Link to="/create">Create Question</Link></li>
+                    <li><Link to="/interview">Start Interview</Link></li>
+                  </ul>
+
+                  <Route exact path="/" component={Language}/>
+                  <Route path="/create" component={QuestionForm}/>
+                  <Route path="/interview" component={StartInterview}/>
+                </div>
+            </Router>
           </div>
         );
     }
