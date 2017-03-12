87ceea1f1156f23ebd5578365d9180c54bde2c5a
JR Meunrath <jmeunrath@overstock.com>
2017-03-12 11:07:18 -0700

    UI changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,27 @@ import Language from './components/Language/Language';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to ReView</h2>
-        </div>
-        <Language />
-      </div>
-    );
-  }
+    constructor() {
+        super()
+        this.state = {
+            isSelected: true
+        }
+    }
+
+    selectLanguage() {
+
+    }
+
+    render() {
+        return (
+          <div className="App">
+            <div className="App-header" >
+              <h2>Welcome to ReView</h2>
+            </div>
+            <Language languageEvent={this.selectLanguage} />
+          </div>
+        );
+    }
 }
 
 export default App;
